4ea12d0cea84cc3914c6b24eedff638bc7cf0453
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-05-23 19:04:14 +0200

    1

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <HelmetProvider>
           <App />
         </HelmetProvider>
